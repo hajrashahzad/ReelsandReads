07cde784ec42ab0c7a0e63827e2c04541982a34a
Hajra <hshahzad.bscs20seecs@seecs.edu.pk>
2022-05-31 02:02:54 +0500

    Small changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,9 +12,7 @@ class HomeController extends Controller
         // DB::insert('insert into users (username, passwd,email) values (?,?,?)', [
         //     'haj', 'password','me@gmail.com', 
         // ]);
-       // $res = DB::select('SELECT photoURL, title FROM items WHERE item_type = ? and item_id IN (SELECT movie_id FROM movielist WHERE username = ?)', ['movie','test1']);
-       $res = DB::table('movielist')->where('username', 'test1')->value('movie_id'); 
-       $movielist = DB::table('item')->where('')
-       return view('welcome', ['res' => $res]);
+       $res = DB::select('SELECT photoURL, title FROM items WHERE item_type = ? and item_id IN (SELECT movie_id FROM movielist WHERE username = ?)', ['movie','test1']);
+       return view('home', ['res' => $res]);
     }
 }
