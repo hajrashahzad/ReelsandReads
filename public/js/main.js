1a38aaf4356ea3f99ae70ae7fb1b251cbbffdf99
baqirmuhammad <bmuhammad.bscs20seecs@seecs.edu.pk>
2022-06-01 06:33:43 +0500

    finalised the proj

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,44 @@
 
-function showToggle(){
+function showToggle() {
     const primaryNav = document.querySelector(".primary-navigation");
     const visibility = primaryNav.getAttribute('data-visible');
     var btn = document.getElementsByClassName('mobile-nav-toggle');
-    if(visibility == 'false'){
-        primaryNav.setAttribute('data-visible', true); 
+    if (visibility == 'false') {
+        primaryNav.setAttribute('data-visible', true);
         document.getElementById('toggle-button').innerHTML = '<i class="fa-solid fa-xmark fa-2x"></i>';
     }
-    else if (visibility == 'true'){
-        primaryNav.setAttribute('data-visible', false); 
+    else if (visibility == 'true') {
+        primaryNav.setAttribute('data-visible', false);
         document.getElementById('toggle-button').innerHTML = '<i class="fa-solid fa-bars fa-2x"></i>';
     }
 }
 var width = window.innerWidth;
 const searchbutton = document.querySelector("#searchbutton");
 var searchbuttonVisibilty = searchbutton.getAttribute('button-visible');
-$(document).ready(function(){
-    if(width>557){
+$(document).ready(function () {
+    if (width > 557) {
         searchbuttonVisibilty.setAttribute('button-visible', true);
     }
-    else if(width <= 557){
+    else if (width <= 557) {
         searchbuttonVisibilty.setAttribute('button-visible', false);
     }
 });
-function resizeAction(){
+function resizeAction() {
     width = window.innerWidth;
-    if(width>557){
+    if (width > 557) {
         searchbuttonVisibilty.setAttribute('button-visible', true);
     }
-    else if(width <= 557){
+    else if (width <= 557) {
         searchbuttonVisibilty.setAttribute('button-visible', false);
     }
 }
-function displayBar(){
+function displayBar() {
     const bar = document.querySelector("#searchbar");
     const barVisibility = bar.getAttribute("bar-visible");
-    if(barVisibility == 'true'){
+    if (barVisibility == 'true') {
         bar.setAttribute('bar-visible', false)
     }
-    else if (barVisibility == 'false'){
+    else if (barVisibility == 'false') {
         bar.setAttribute('bar-visible', true);
     }
 }
@@ -53,8 +53,7 @@ function handleBooksOptionClick(e) {
         $("#books-selected").show();
         $('#books-option').children(".option-name")[0].style = "color: white"
         const svgContent = $("#books-icon").children();
-        for (let i = 0; i < svgContent.length; i++)
-        {
+        for (let i = 0; i < svgContent.length; i++) {
             svgContent[i].attributes.fill.nodeValue = "white";
         }
 
@@ -64,11 +63,10 @@ function handleBooksOptionClick(e) {
         $("#books-selected").hide();
         $('#books-option').children(".option-name")[0].style = "color: #326280"
         const svgContent = $("#books-icon").children();
-        for (let i = 0; i < svgContent.length; i++)
-        {
+        for (let i = 0; i < svgContent.length; i++) {
             svgContent[i].attributes.fill.nodeValue = "#326280";
         }
-    }   
+    }
 }
 
 function handleMoviesOptionClick(e) {
@@ -79,8 +77,7 @@ function handleMoviesOptionClick(e) {
         $("#movies-selected").show();
         $('#movies-option').children(".option-name")[0].style = "color: white"
         const svgContent = $("#movies-icon").children();
-        for (let i = 0; i < svgContent.length; i++)
-        {
+        for (let i = 0; i < svgContent.length; i++) {
             svgContent[i].attributes.fill.nodeValue = "white";
         }
     }
@@ -89,11 +86,10 @@ function handleMoviesOptionClick(e) {
         $("#movies-selected").hide();
         $('#movies-option').children(".option-name")[0].style = "color: #326280"
         const svgContent = $("#movies-icon").children();
-        for (let i = 0; i < svgContent.length; i++)
-        {
+        for (let i = 0; i < svgContent.length; i++) {
             svgContent[i].attributes.fill.nodeValue = "#326280";
         }
-    }   
+    }
 }
 
 function handleAnimeOptionClick(e) {
@@ -115,21 +111,18 @@ function handleBooksOptionHoverIn(e) {
     $("#books-option").css("background", "#326280")
     $('#books-option').children(".option-name")[0].style = "color: white"
     const svgContent = $("#books-icon").children();
-    for (let i = 0; i < svgContent.length; i++)
-    {
+    for (let i = 0; i < svgContent.length; i++) {
         svgContent[i].attributes.fill.nodeValue = "white";
     }
 }
 
 function handleBooksOptionHoverOut(e) {
     e.preventDefault();
-    if ($("#books-selected").css("display") === 'none')
-    {
+    if ($("#books-selected").css("display") === 'none') {
         $("#books-option").css("background", "rgba(255, 255, 255, 0.4")
         $('#books-option').children(".option-name")[0].style = "color: #326280"
         const svgContent = $("#books-icon").children();
-        for (let i = 0; i < svgContent.length; i++)
-        {
+        for (let i = 0; i < svgContent.length; i++) {
             svgContent[i].attributes.fill.nodeValue = "#326280";
         }
     }
@@ -140,8 +133,7 @@ function handleMoviesOptionHoverIn(e) {
     $("#movies-option").css("background", "#326280")
     $('#movies-option').children(".option-name")[0].style = "color: white"
     const svgContent = $("#movies-icon").children();
-    for (let i = 0; i < svgContent.length; i++)
-    {
+    for (let i = 0; i < svgContent.length; i++) {
         svgContent[i].attributes.fill.nodeValue = "white";
     }
 }
@@ -152,8 +144,7 @@ function handleMoviesOptionHoverOut(e) {
         $("#movies-option").css("background", "rgba(255, 255, 255, 0.4")
         $('#movies-option').children(".option-name")[0].style = "color: #326280"
         const svgContent = $("#movies-icon").children();
-        for (let i = 0; i < svgContent.length; i++)
-        {
+        for (let i = 0; i < svgContent.length; i++) {
             svgContent[i].attributes.fill.nodeValue = "#326280";
         }
     }
@@ -182,7 +173,7 @@ function addGenre(e) {
     let newGenre = "<div class='selected-genre'>";
     newGenre += "<div class='genre-name' value=" + genreVal + ">" + selectedGenre + "</div>";
     newGenre += "<div class='genre-delete-container'>";
-    newGenre += "<button class='genre-delete'><img src='../images/DeleteIcon.svg'></button>";
+    newGenre += "<button class='genre-delete' id='Delete'><img src='../images/DeleteIcon.svg'></button>";
     newGenre += "</div>";
     newGenre += "</div>";
     $("#selected-genres").append(newGenre);
@@ -192,7 +183,7 @@ function deleteGenre(e) {
     e.preventDefault();
     const clickedButton = e.currentTarget;
     clickedButton.parentElement.parentElement.remove();
-    if($("#selected-genres").children().length === 1)
+    if ($("#selected-genres").children().length === 1)
         $("#no-genre").show();
 }
 
@@ -200,20 +191,18 @@ function handleNextClick(e) {
     e.preventDefault();
     const carousel = $(".main-carousel").data('flickity');
     const selectedIndex = carousel.selectedIndex
-    if (selectedIndex === 1)
-    {
+    if (selectedIndex === 1) {
         console.log("Submit");
         let selectedGenres = [];
         let selectedGenreIDs = [];
-        let books=0, movies=0, anime=0;
-        let _token   = $('meta[name="csrf-token"]').attr('content');
+        let books = 0, movies = 0, anime = 0;
+        let _token = $('meta[name="csrf-token"]').attr('content');
         const genres = $("#selected-genres").children();
         if (genres.length > 1) {
-            for (let i = 1; i < genres.length; i++)
-            {
+            for (let i = 1; i < genres.length; i++) {
                 selectedGenres.push(genres[i].children[0].innerText);
                 selectedGenreIDs.push(genres[i].children[0].getAttribute('value'));
-            } 
+            }
         }
         if ($("#books-selected").css("display") !== "none")
             books = 1;
@@ -233,17 +222,16 @@ function handleNextClick(e) {
                 anime,
                 _token
             },
-            success: function(response) {
+            success: function (response) {
                 console.log(response);
                 window.location.pathname = response.redirectURL;
             },
-            error: function(error) {
+            error: function (error) {
                 console.log(error);
             }
         })
     }
-    else
-    {
+    else {
         $("#prev-button").show();
         $(".page-heading")[0].textContent = "Choose your preferred genres";
         $(".main-carousel").flickity('next', false, false);
